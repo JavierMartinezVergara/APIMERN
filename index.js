353c32d6ee41fbcc9841987523f94d510bec2447
JavierMartinezVergara <javierrrr.15@gmail.com>
2019-08-27 18:35:23 -0500

    javier

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
+
+const { mongoose } = require('./database');
 
 //Configuracion
 app.set('port', process.env.PORT ||3000)
+
+
 //Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 //Routes
+app.use('/api/tasks', require('./routes/task.routes'));
+
+
 
 //Static files
+//Escribe la ruta de donde esta alojado sin importar SO
+//console.log(path.join(__dirname,'/public'));
+app.use(express.static(path.join(__dirname,'/public')));
 
 //Init
 app.listen(app.get('port'), ()=>{
-    console.log('Server en puerto 3000');
-})
+    console.log(`Server en puerto ${app.get('port')}`);
+});
